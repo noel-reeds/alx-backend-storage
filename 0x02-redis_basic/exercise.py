3a911d8ed3a5a5237f814a621d690f1d226f015f
altoid <noelreeds01@gmail.com>
2024-09-30 13:32:12 +0000

    custom get and fn to parametrize

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,10 +18,24 @@ class Cache:
         self._redis.set(key, data)
         return key
     
-    def get(self, key: str, fn: Optional[Callable]) -> str:
+    def get(self, key: str, fn: Optional[Callable] = None):
         """simulates redis.Redis get method"""
         # check if key exists.
         if key in self._redis.keys():
-            self._redis.get(key)
+            value = self._redis.get(key)
+            if fn:
+                return fn(value)
         else:
             return None
+
+    def get_str(self, key: str) -> str:
+        """converts data retrieved from cache to string"""
+        if self._redis.type(key) is str:
+            value = self.get(key)
+            return str(value)
+
+    def get_int(self, key: str) -> int:
+        """converts data retrieved from cache to integer"""
+        if self._redis.type(key) is int:
+            value = self.get(key)
+            return int(value)
