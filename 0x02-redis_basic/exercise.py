09de0341a1888d95cc39c693cbe0823955725810
altoid <noelreeds01@gmail.com>
2024-10-03 13:40:28 +0000

    stores inputs and outputs of a function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,8 +16,24 @@ def count_calls(method: Callable) -> Callable:
         res = method(self, *args, **kwargs)
         self._redis.incr(key, amount=1)
         return res
+
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """Store the history of inputs and outputs for a function"""
+    inputkey = method.__qualname__ +':inputs'
+    outputkey = method.__qualname__ + 'outputs'
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(outputkey, res)
+        for arg in args:
+            self._redis.rpush(inputkey, str(arg))
+        return res
+
+    return wrapper
 
 class Cache:
     """class cache"""
@@ -27,6 +43,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores a str in a redis database"""
         key = str(uuid.uuid4())
