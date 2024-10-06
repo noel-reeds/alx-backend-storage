917aaf1f4f97dba62c3c72ed2669218322fadeae
altoid <noelreeds01@gmail.com>
2024-10-06 12:10:51 +0000

    tracks number of times a web page has been accessed

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""An expiring web cache and tracker"""
+import redis
+import requests
+from functools import wraps
+
+
+def get_page(url: str) -> str:
+    """implementation of an expiring web cache and tracker"""
+    r = redis.Redis()
+    res = requests.get(url)
+    cache_name = f"count:{url}"
+
+    if res.status_code == 200:
+        r.incr(cache_name, amount=1)
+        r.expire(cache_name, 10)
+        return res.text
