085b8bf849b725057f4463c9ca148154173facb9
altoid <noelreeds01@gmail.com>
2024-10-06 16:52:56 +0000

    added decorator to count number of calls to page

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,15 +2,28 @@
 """An expiring web cache and tracker"""
 import redis
 import requests
+from typing import Callable
 from functools import wraps
 
 
+def calls_counter(method: Callable) -> Callable:
+    """Returns number of times page is accessed"""
+    cache_name = f"count:{url}"
+
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """wrapper function"""
+        res = method(*args)
+        r = redis.Redis()
+        r.incr(cache_name, amount=1)
+        r.expire(cache_name, 10)
+        return res
+    return wrapper
+
+@calls_counter
 def get_page(url: str) -> str:
     """implementation of an expiring web cache and tracker"""
     r = redis.Redis()
     res = requests.get(url)
-    cache_name = f"count:{url}"
 
-    r.incr(cache_name, amount=1)
-    r.expire(cache_name, 10)
     return res.text
