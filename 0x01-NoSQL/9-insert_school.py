a2b15d989418abb4f4ec4b10dd8271d6384048c0
altoid <noelreeds01@gmail.com>
2024-10-22 07:26:39 +0000

    inserts a new document into Mongo server

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Inserts a document into a mongod server"""
+
+def insert_school(mongo_collection, **kwargs):
+    """Function def"""
+    new_document = mongo_collection.insert_one(**kwargs)
+    new_id = new_document.get('_id')
+    return new_id
